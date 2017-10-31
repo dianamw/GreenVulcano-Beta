8288fc916ee0024c65feab7778202238561c0e65
dianamw <32761660+dianamw@users.noreply.github.com>
2017-10-31 15:57:25 +0100

    Update sidebar.js

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -6,3 +6,18 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
 }
+
+ var acc = document.getElementsByClassName("accordion");
+      var i;
+
+      for (i = 0; i < acc.length; i++) {
+        acc[i].onclick = function() {
+          this.classList.toggle("active");
+          var panel = this.nextElementSibling;
+          if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+          } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+          }
+        }
+      }
