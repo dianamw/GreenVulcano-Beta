bbccacc885f7ed4a58a148267e9226dfc650054c
dianamw <32761660+dianamw@users.noreply.github.com>
2017-11-03 11:51:47 +0100

    Update sidebar.js

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,3 +1,20 @@
+ var hidden = false;
+
+function nav()
+ {
+ if(hidden == false)
+ {
+     closeNav();
+     hidden  = true;
+ }
+  else
+   {
+    openNav();
+    hidden = false; 
+  } 
+ }
+
+
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
