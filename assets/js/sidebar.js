510469f4707d1567e962bc46f61ed3c4d3f37261
dianamw <32761660+dianamw@users.noreply.github.com>
2017-11-07 10:21:03 +0100

    Update sidebar.js

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,16 +1,16 @@
-/*function openNav() {
+function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
 }
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
-}*/
+}
 
-$(document).ready(function(){
+/*$(document).ready(function(){
         $("#slide-toggle").click(function(){
             $(".sidenav").animate({
                 width: "toggle"
             });
         });
-    });
+    }); */
