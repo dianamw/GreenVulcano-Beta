20af2c730c2287a0413875cadd6ecc4e3b9d7405
dianamw <32761660+dianamw@users.noreply.github.com>
2017-11-08 11:49:10 +0100

    Update sidebar.js

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,11 +1,26 @@
-function openNav() {
+function toggle(){
+  if(!sidebar){
+  		sidebar = true;
+        document.getElementById("mySidenav").style.width = "250px";
+    	document.getElementById("main").style.marginLeft = "250px";
+       return;
+  }
+  
+  if(sidebar){
+  	sidebar = false;
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  	}
+  }
+
+/*function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
 }
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
-}
+} */
 
 /*$(document).ready(function(){
         $("#slide-toggle").click(function(){
