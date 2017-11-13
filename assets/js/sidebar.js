43b7174a903854757194de13892b27c3359f0170
dianamw <32761660+dianamw@users.noreply.github.com>
2017-11-13 10:23:01 +0100

    Update sidebar.js

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -3,6 +3,8 @@ function toggle(){
   		sidebar = true;
         document.getElementById("mySidenav").style.width = "250px";
     	document.getElementById("main").style.marginLeft = "250px";
+    	document.getElementById("headerP").style.marginLeft = "250px";
+    
        return;
   }
   
@@ -10,6 +12,7 @@ function toggle(){
   	sidebar = false;
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
+    document.getElementById("headerP").style.marginLeft = "0";
   	}
   }
 
