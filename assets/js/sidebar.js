32557f3f76b04cecd9d43627a6092f98ecc4880e
dianamw <32761660+dianamw@users.noreply.github.com>
2017-11-16 16:25:33 +0100

    Update sidebar.js

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,17 +1,33 @@
-/*function toggle(){
-  if(!sidebar){
-  		sidebar = true;
-        document.getElementById("mySidenav").style.width = "250px";
-    	document.getElementById("main").style.marginLeft = "250px";
-       return;
+//script toggle sidebar
+var sidebar = false;
+ function toggle(){
+   if(!sidebar){
+       sidebar = true;
+       document.getElementById("mySidenav").style.width = "250px";
+       document.getElementById("main").style.marginLeft = "250px";
+        return;
+   }
+   
+   else if (sidebar){
+     sidebar = false;
+     document.getElementById("mySidenav").style.width = "0";
+     document.getElementById("main").style.marginLeft = "0";
+   	}
+   }
+   
+  var acc = document.getElementsByClassName("accordion");
+  var i;
+  for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function() {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+    }
   }
-  
-  if(sidebar){
-  	sidebar = false;
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-  	}
-  }*/
 
 /*function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
