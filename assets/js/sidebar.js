73cae4feeb80d54d1f232217ccff5bd0db0009a6
dianamw <32761660+dianamw@users.noreply.github.com>
2017-12-05 16:31:43 +0100

    Update sidebar.js

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,9 +1,9 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "280px";
+    document.getElementById("sidebar").style.width = "280px";
     document.getElementById("main").style.marginLeft = "280px";
 }
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("sidebar").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
 } 
 //script toggle sidebar
