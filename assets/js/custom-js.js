1c0c56f7e3f37a90afc2bcfdf0ba0ef2c5dba039
dianamw <32761660+dianamw@users.noreply.github.com>
2017-12-12 11:58:32 +0100

    Update custom-js.js

--- assets/js/custom-js.js
+++ assets/js/custom-js.js
@@ -1,3 +1,4 @@
+//Sidebar Open-Close Script
 function openNav() {
     document.getElementById("sidebar").style.width = "280px";
     document.getElementById("main").style.marginLeft = "280px";
@@ -6,10 +7,14 @@ function closeNav() {
     document.getElementById("sidebar").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
 }
+//Hash
 $(document).ready(function () {
 	  if (location.hash){
 	    $(location.hash).collapse('show');
 	    $(location.hash).parents('.accordion-body').collapse('show');
   }
 });
-	
+//Animation Script
+AOS.init({
+  duration: 1200,
+})
