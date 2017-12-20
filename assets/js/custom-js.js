8e78bc96e94736dc47236b3f81456244ca393f43
dianamw <32761660+dianamw@users.noreply.github.com>
2017-12-20 15:41:01 +0100

    Update custom-js.js

--- assets/js/custom-js.js
+++ assets/js/custom-js.js
@@ -1,12 +1,22 @@
 //Sidebar Open-Close Script
-function openNav() {
+/*function openNav() {
     document.getElementById("sidebar").style.width = "280px";
     document.getElementById("main").style.marginLeft = "280px";
 }
 function closeNav() {
     document.getElementById("sidebar").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
-}
+}*/
+
+$('button').toggle(
+
+function() {
+    $('#sidebar').css('right', '0')
+}, function() {
+    $('#main').css('right', '0')
+})
+
+
 $(document).ready(function () {
     //fetch hash in a variable
     var hash = location.hash;
