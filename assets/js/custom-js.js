8b591ebcbf7a6788e6994bfa4bd7e700c95064cb
dianamw <32761660+dianamw@users.noreply.github.com>
2017-12-20 16:55:35 +0100

    Update custom-js.js

--- assets/js/custom-js.js
+++ assets/js/custom-js.js
@@ -1,12 +1,12 @@
 //Sidebar Open-Close Script
-/*function openNav() {
+function openNav() {
     document.getElementById("sidebar").style.width = "280px";
     document.getElementById("main").style.marginLeft = "280px";
 }
 function closeNav() {
     document.getElementById("sidebar").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
-}*/
+}
 
 $('button').onclick(
 
