05a92475efa968aca066913a010541ec5586f6bc
dianamw <32761660+dianamw@users.noreply.github.com>
2018-01-12 12:52:11 +0100

    Update custom-js.js

--- assets/js/custom-js.js
+++ assets/js/custom-js.js
@@ -25,5 +25,16 @@ $(document).ready(function () {
        		 'color' : '#ffffff'
       });
 
+	$(function(){
+    var current = location.pathname;
+    current = current.substring(current.lastIndexOf('/'));
+    $('sidebar ul li a').each(function(){
+        var $this = $(this);
+        // if the current path is like this link, make it active
+        if($this.attr('href').indexOf(current) !== -1){
+            $this.addClass('active');
+        }
+    });
+});
 });
 
