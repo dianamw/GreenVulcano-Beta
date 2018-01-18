5b95a84a8ab22677c4bf8f92bbb5a58b610632f3
dianamw <32761660+dianamw@users.noreply.github.com>
2018-01-18 12:13:15 +0100

    Update custom-js.js

--- assets/js/custom-js.js
+++ assets/js/custom-js.js
@@ -23,11 +23,11 @@ $(document).ready(function() {
   $('#sidebar ul li a[href^="' + hash + '"]').parent().addClass('active');
 
   /* fetch path without hash */
-  var full_path = location.href.split("#")[1];
+  var full_path = location.href.split("#")[0];
   /* add active class to links */
   $("#sidebar ul ul a").each(function() {
     var $this = $(this);
-    if ($this.prop("href").split("#")[1] == full_path) {
+    if ($this.prop("href").split("#")[0] == full_path) {
       $(this).parent().addClass("active");
     }
   });
